58dc3586e87162a4c7c694ab3b9bbc4f3f8d5892
Deni Setiya <144502025+denisetiya@users.noreply.github.com>
2023-11-26 17:43:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/assets', express.static('assets'));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://vs-class-3b.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 
 // log user
 const loginMhs = require('./router/login/loginMhs')
