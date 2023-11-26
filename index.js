c37457090e2318f0642b70b629ab2c0cb90fda6a
SisfoProject <150670473+SisfoProject@users.noreply.github.com>
2023-11-27 06:33:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/assets', express.static('assets'));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://vs-class-3b.vercel.app');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
